aa29c4619a5f917ee4080b6d1d36b9e344b758c0
Andrii Tykhonov <tihonav@gmail.com>
2017-10-31 18:42:29 +0100

    increased verbosity

--- stl_gdml.py
+++ stl_gdml.py
@@ -675,7 +675,7 @@ if "--materials" in sys.argv:
 if len(sys.argv)<3:
 	__print__('Not enough argumnets provided! see  "python %s -h" for more details'%MODULE_NAME)
 	raise SystemExit
-matchnames = [item for item in sys.argv[2:] if sys.argv[1].lower() in item.lower() or sys.argv[1].lower().split('.gdml')[0] in item.lower()]
+matchnames = [item for item in sys.argv[2:] if sys.argv[1].lower().split('.gdml')[0] == item.lower().split('.stl')[0]]
 if matchnames:
 	__print__('[out_name] should not coincide with one of the input .stl file names  "python %s -h" for more details'%MODULE_NAME)
 	__print__('[out_name] = ' + sys.argv[1] + ' mathces with ' + matchnames[0] + ' - please use different name for the [out_name], for example "top.gdml"')
@@ -683,7 +683,7 @@ if matchnames:
 if ".stl" in sys.argv[1]:
 	__print__('Please provide output file name! see  "python %s -h" for more details'%MODULE_NAME)
         raise SystemExit
-creat_gdml_bundle(sys.argv[1],sys.argv[2:])
+creat_gdml_bundle(sys.argv[1].split('.gdml')[0],sys.argv[2:])
 	
 	
 	
